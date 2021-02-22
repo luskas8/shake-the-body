b39019db0e16b915299302474ee229182cf3148e
Luskas8 <gnomo1049@gmail.com>
2021-02-22 10:43:40 -0300

    Inicio do projeto em React, primeiro componente ExperienceBar

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ExperienceBar } from "./components/ExperienceBar";
+
+function App() {
+  return <div>
+    <ExperienceBar />
+  </div>;
+}
+
+export default App;
---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,8 @@
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <span>600 xp</span>
+    </header>
+  );
+}
