d79ca9ba46f5ab002e0721aee95b7d021a265c2f
Luskas8 <gnomo1049@gmail.com>
2021-02-23 16:49:30 -0300

    Add countdown decrease time function and css

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,17 +1,45 @@
+import { useState, useEffect } from 'react'
 import styles from '../styles/components/Countdown.module.css'
 
 export function Coutdown() {
+  const [time, setTime] = useState(25 * 60)
+  const [active, setActive] = useState(false)
+
+  const minutes = Math.floor(time / 60)
+  const seconts = time % 60
+
+  const [minLeft, minRight] = String(minutes).padStart(2, '0').split('')
+  const [secLeft, secRight] = String(seconts).padStart(2, '0').split('')
+
+  function startCountdown() {
+    setActive(true)
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
+    }
+  }, [active, time])
+
   return (
-    <div className={styles.countdownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minLeft}</span>
+          <span>{minRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secLeft}</span>
+          <span>{secRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+
+      <button type="button" className={styles.countdownButton} onClick={startCountdown}>
+        Iniciar um ciclo
+    </button>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
 import { CompletedChallengers } from '../components/CompletedChallengers'
+import { Coutdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 import styles from '../styles/pages/Home.module.css'
 
-export default function Home () {
+import Head from 'next/head'
+
+export default function Home() {
   return (
     <div className={styles.container}>
+      <Head><title>Início | Shake the Body</title></Head>
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallengers />
+          <Coutdown />
         </div>
         <div>
-          
         </div>
       </section>
     </div>
