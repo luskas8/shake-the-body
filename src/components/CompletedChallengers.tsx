fea40c5706cc6ed675b03fd97a6f41f21b949807
Luskas8 <gnomo1049@gmail.com>
2021-02-24 14:58:27 -0300

    Link challengers contexts to completedchallengers component

--- src/components/CompletedChallengers.tsx
+++ src/components/CompletedChallengers.tsx
@@ -1,12 +1,14 @@
 // eslint-disable-next-line no-use-before-define
-import React from 'react'
+import React, { useContext } from 'react'
+import { ChallengersContext } from '../contexts/ChallengersContexts'
 import styles from '../styles/components/CompletedChallengers.module.css'
 
 export function CompletedChallengers () {
+  const { challengersCompleted } = useContext(ChallengersContext)
   return (
     <div className={styles.completedChallengersContainer}>
       <span>Desafios completos</span>
-      <span>5</span>
+      <span>{challengersCompleted}</span>
     </div>
   )
 }
