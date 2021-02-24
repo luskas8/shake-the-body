3083f5bbc557a05006fb545abd2cfb794728777a
Luskas8 <gnomo1049@gmail.com>
2021-02-24 09:50:00 -0300

    Change const active to isActive at Countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/components/Countdown.module.css'
 
 export function Coutdown() {
   const [time, setTime] = useState(25 * 60)
-  const [active, setActive] = useState(false)
+  const [isActive, setIsActive] = useState(false)
 
   const minutes = Math.floor(time / 60)
   const seconts = time % 60
@@ -12,16 +12,16 @@ export function Coutdown() {
   const [secLeft, secRight] = String(seconts).padStart(2, '0').split('')
 
   function startCountdown() {
-    setActive(true)
+    setIsActive(true)
   }
 
   useEffect(() => {
-    if (active && time > 0) {
+    if (isActive && time > 0) {
       setTimeout(() => {
         setTime(time - 1)
       }, 1000)
     }
-  }, [active, time])
+  }, [isActive, time])
 
   return (
     <div>
