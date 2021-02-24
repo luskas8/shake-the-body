edfb22b112bb705659c9f74594500f3d17a027e5
Luskas8 <gnomo1049@gmail.com>
2021-02-24 14:57:31 -0300

    Link challengers contexts to profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,10 @@
 // eslint-disable-next-line no-use-before-define
-import React from 'react'
+import React, { useContext } from 'react'
+import { ChallengersContext } from '../contexts/ChallengersContexts'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile () {
+  const { level } = useContext(ChallengersContext)
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/luskas8.png" alt="Lucas Anjos" />
@@ -10,7 +12,7 @@ export function Profile () {
         <strong>Lucas Anjos</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
