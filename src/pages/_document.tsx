ddad2e34037a8641c676097c27227431b7cc71d6
Lucas Anjos <42415782+luskas8@users.noreply.github.com>
2021-02-26 16:12:20 -0300

    Add language meta content

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta http-equiv="Content-Language" content="pt-br">
+          
           <link rel="shortcut icon" href="favicon.png" type="image/png"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
